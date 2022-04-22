0a8e14556bda6d06017abc0a3894246d7f42206e
MissUnique <simply.uniqu@hotmail.com>
2022-04-22 18:59:04 +0200

    Update PlayerMovement

--- Assets/Scripts/NewPlayerMovement.cs
+++ Assets/Scripts/NewPlayerMovement.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class NewPlayerMovement : MonoBehaviour
@@ -13,48 +12,33 @@ public class NewPlayerMovement : MonoBehaviour
     float forwardSteps;
     float extraSteps;
 
-    //public float speed = 10f;
-    //Rigidbody rb;
-
     // Start is called before the first frame update
     void Start()
     {
         isTossed = false;
-
-        //rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log("isTossed: " + isTossed);
-        //Debug.Log("NewDiceTossing.diceVelocity: " + NewDiceTossing.diceVelocity);
-
         currentPlayerPosition = transform.position;
         newPlayerPosition = currentPlayerPosition;
 
         if (isTossed)
         {
-            //Debug.Log("diceNumber: " + NewDiceNumberText.diceNumber);
             isTossed = false;
 
             // Check if player is in the 10 floors playground
             if (currentPlayerPosition.y < 10)
             {
-                //Debug.Log("Player is in the 10 floors playground");
-
                 // Player is in odd levels
                 if ((currentPlayerPosition.y - 0.75) % 2 == 0)
                 {
-                    //Debug.Log("Player is in odd levels");
-
                     newPlayerPosition.x += NewDiceNumberText.diceNumber;
 
                     // Right movement
                     if (newPlayerPosition.x > 0 && newPlayerPosition.x < 11)
                     {
-                        //Debug.Log("currentPlayerPosition: " + currentPlayerPosition);
-                        //Debug.Log("newPlayerPosition: " + newPlayerPosition);
                         StartCoroutine(moveObject(currentPlayerPosition, newPlayerPosition, 0f));
                     }
 
@@ -62,14 +46,11 @@ public class NewPlayerMovement : MonoBehaviour
                     else if (newPlayerPosition.x > 10)
                     {
                         forwardSteps = 10 - currentPlayerPosition.x;
-                        //Debug.Log("forwardSteps: " + forwardSteps);
                         extraSteps = newPlayerPosition.x - 10;
-                        //Debug.Log("extraSteps:" + extraSteps);
 
                         tempPlayerPosition = currentPlayerPosition;
                         tempPlayerPosition.x += forwardSteps;
                         StartCoroutine(moveObject(currentPlayerPosition, tempPlayerPosition, extraSteps));
-
                     }
                 }
 
@@ -84,11 +65,18 @@ public class NewPlayerMovement : MonoBehaviour
                         StartCoroutine(moveObject(currentPlayerPosition, newPlayerPosition, 0f));
                     }
 
+                    // Next level movement
+                    else if (newPlayerPosition.x < 1)
+                    {
+                        forwardSteps = currentPlayerPosition.x - 1;
+                        extraSteps = 1 - newPlayerPosition.x;
 
+                        tempPlayerPosition = currentPlayerPosition;
+                        tempPlayerPosition.x -= forwardSteps;
+                        StartCoroutine(moveObject(currentPlayerPosition, tempPlayerPosition, extraSteps));
+                    }
                 }
-
             }
-            
         }
     }
     
@@ -118,7 +106,6 @@ public class NewPlayerMovement : MonoBehaviour
         Vector3 newPosition = currentPosition;
         newPosition.y++;
         newPosition.z++;
-        Debug.Log("Start moving up..");
         float totalMovementTime = 1f;
         float currentMovementTime = 0f;
         while (transform.localPosition != newPosition)
@@ -132,7 +119,12 @@ public class NewPlayerMovement : MonoBehaviour
         if (extraStepsUP > 0)
         {
             currentPosition = newPosition;
-            newPosition.x -= extraStepsUP;
+            // If player on even level -> moves left
+            if ((currentPosition.y - 0.75) % 2 != 0)
+                newPosition.x -= extraStepsUP;
+            // If player on odd level -> moves right
+            else if ((currentPosition.y - 0.75) % 2 == 0)
+                newPosition.x += extraStepsUP;
             StartCoroutine(moveObject(currentPosition, newPosition, 0f));
         }
     }
